9f6833d37c6d3a2688a2f0e2aa1967b2e5e839aa
FilippoRossoni <filippo.rossoni@gmail.com>
2020-09-16 00:32:43 +0200

    add Microprofile Rest Module

---
+++ patternRecognitionRest/src/main/java/it/filippor/welld/test/pattern/recognition/rest/DemoRestApplication.java
@@ -0,0 +1,11 @@
+package it.filippor.welld.test.pattern.recognition.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemoRestApplication extends Application {
+}
